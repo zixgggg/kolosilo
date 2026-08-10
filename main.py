cdbaa715fd153afa39923508457310d8dcaf6929
zixgggg <zixzix035@gmail.com>
2026-08-10 22:16:39 +0800

    turn bar to class

--- main.py
+++ main.py
@@ -43,11 +43,14 @@ class BarClass(QtWidgets.QWidget):
         #input_line.setText("a")
         
         self.close_bar_rbutton=QtWidgets.QRadioButton("bar")
-        self.close_bar_rbutton.clicked.connect(lambda:self.setVisible(not bar.isVisible()))
-        self.back_page_button.clicked.connect(webviwe.back)
-        self.forward_page_button.clicked.connect(webviwe.forward)
-        self.reload_page_button.clicked.connect(webviwe.reload)
-        self.enter_page_button.clicked.connect(lambda:webviwe(google+search_input_line.displayText()))
+        self.close_bar_rbutton.clicked.connect(lambda:self.setVisible(not self.isVisible()))
+        self.back_page_button.clicked.connect(webview.back)
+        self.forward_page_button.clicked.connect(webview.forward)
+        self.reload_page_button.clicked.connect(webview.reload)
+        self.enter_page_button.clicked.connect(lambda:webview.load(QtCore.QUrl(google+self.search_input_line.displayText())))
+        #self.search_input_line.returnPressed.connect(lambda:webview.load(QtCore.QUrl(google+self.search_input_line.displayText())))
+        self.search_input_line.returnPressed.connect(self.enter_page_button.click)
+        
 
         self.back_page_button.setFixedSize(50,20)
         self.forward_page_button.setFixedSize(50,20)
@@ -60,83 +63,20 @@ class BarClass(QtWidgets.QWidget):
         search_line_layout=QtWidgets.QHBoxLayout()
         
         #b_layout.addWidget(back_page_button,alignment=QtCore.Qt.AlignmentFlag.AlignLeft)
-        page_button_layout.addWidget(back_page_button)
-        page_button_layout.addWidget(forward_page_button)
-        page_button_layout.addWidget(reload_page_button)
+        page_button_layout.addWidget(self.back_page_button)
+        page_button_layout.addWidget(self.forward_page_button)
+        page_button_layout.addWidget(self.reload_page_button)
         page_button_layout.addStretch()
         bar_layout.addLayout(page_button_layout)
         #back_page_button.hide()
         
-        search_line_layout.addWidget(search_input_line)
-        search_line_layout.addWidget(enter_page_button)
+        search_line_layout.addWidget(self.search_input_line)
+        search_line_layout.addWidget(self.enter_page_button)
         search_line_layout.addStretch()
         bar_layout.addLayout(search_line_layout)
         bar_layout.addStretch()
         self.setLayout(bar_layout)
-bar=QtWidgets.QWidget()
-bar.hide()
-"""
-class BackPageButtonClass(QtWidgets.QPushButton):
-    def __init__(self,text,parent)
-        super().__init__()
-        self.setText(text)
-"""
-back_page_button=QtWidgets.QPushButton("back",parent=bar)
-#back_page_button.setGeometry(0,0,100,50)
-forward_page_button=QtWidgets.QPushButton("forward",parent=bar)
-reload_page_button=QtWidgets.QPushButton("reload",parent=bar)
-enter_page_button=QtWidgets.QPushButton("enter",parent=bar)
-search_input_line=QtWidgets.QLineEdit(parent=bar)
-search_input_line.setPlaceholderText("search")
-#url_input_line=QtWidgets.QLineEdit()
-#input_line.setText("a")
-def toggle_bar():
-    """
-    if bar.isVisible()==True:
-        bar.hide()
-    """
-    bar.setVisible(not bar.isVisible())
-close_bar_rbutton=QtWidgets.QRadioButton("bar")
-close_bar_rbutton.clicked.connect(toggle_bar)
-
-
-back_page_button.clicked.connect(browser.back)
-forward_page_button.clicked.connect(browser.forward)
-reload_page_button.clicked.connect(browser.reload)
-google="https://www.google.com/search?q="
-duckduckgo="https://duckduckgo.com/?q="
-bing="https://www.bing.com/search?q="
-def load_page(engine,q):
-    browser.load(QtCore.QUrl(engine+q))
-enter_page_button.clicked.connect(lambda:load_page(google,search_input_line.displayText()))
-enter_page_button.clicked.connect(webviwe(google+search_input_line.displayText()))
-search_input_line.returnPressed.connect(lambda:load_page(google,search_input_line.displayText()))
-#url_input_line.returnPressed.connect(lambda:load_page(google,input_line.displayText()))
-
-back_page_button.setFixedSize(50,20)
-forward_page_button.setFixedSize(50,20)
-reload_page_button.setFixedSize(50,20)
-enter_page_button.setFixedSize(50,20)
-search_input_line.setFixedSize(150,20)
-
-bar_layout=QtWidgets.QVBoxLayout()
-page_button_layout=QtWidgets.QHBoxLayout()
-search_line_layout=QtWidgets.QHBoxLayout()
-
-#b_layout.addWidget(back_page_button,alignment=QtCore.Qt.AlignmentFlag.AlignLeft)
-page_button_layout.addWidget(back_page_button)
-page_button_layout.addWidget(forward_page_button)
-page_button_layout.addWidget(reload_page_button)
-page_button_layout.addStretch()
-bar_layout.addLayout(page_button_layout)
-#back_page_button.hide()
-
-search_line_layout.addWidget(search_input_line)
-search_line_layout.addWidget(enter_page_button)
-search_line_layout.addStretch()
-bar_layout.addLayout(search_line_layout)
-bar_layout.addStretch()
-bar.setLayout(bar_layout)
+bar=BarClass()
 
 tab=QtWidgets.QTabWidget(window)
 tab.setTabsClosable(True)
@@ -149,31 +89,18 @@ tab.setUsesScrollButtons(True)
 tab.setElideMode(QtCore.Qt.TextElideMode.ElideRight)
 tab.setStyleSheet("""QTabBar::tab{max-width:150px; min-width:150px}""")
 
-def update_icon(icon,b):
-    #tab.setTabIcon(tab.indexOf(browser),browser.icon())
-    tab.setTabIcon(tab.indexOf(b),icon)
-#    if not icon.isNull()==True:
-#        tab.setTabIcon(tab.indexOf(b),icon)
-"""
-def update_icon( icon, b):
-    idx = tab.indexOf(b)
-    print(f"[Debug] 抓到 Icon！Tab 索引: {idx}, 是否無效: {icon.isNull()}")
-    if idx != -1 and not icon.isNull():
-        tab.setTabIcon(idx, icon)
-"""
-#browser.iconChanged.connect(lambda icon,b=browser: update_icon(icon,b))
-browser.iconChanged.connect(lambda icon,b=browser:tab.setTabIcon(tab.indexOf(b),icon))
+webview.iconChanged.connect(lambda icon,webpage_icon_change=webview:tab.setTabIcon(tab.indexOf(webpage_icon_change),icon))
 def close_tab(index):
-    current_browser=tab.widget(index)
-    current_browser.deleteLater()
+    current_webpage=tab.widget(index)
+    current_webpage.deleteLater()
     tab.removeTab(index)
     if tab.count()==0:
         sys.exit()
 #tab.tabCloseRequested.connect(lambda:tab.removeTab(tab.currentIndex()))
 tab.tabCloseRequested.connect(close_tab)
-title=browser.title()
-browser.titleChanged.connect(lambda:tab.setTabText(tab.currentIndex(),browser.title()))
-tab.addTab(browser,title)
+title=webview.title()
+webview.titleChanged.connect(lambda:tab.setTabText(tab.currentIndex(),webview.title()))
+tab.addTab(webview,title)
 a=QtWidgets.QLabel("123")
 tab.addTab(a,"aa")
 tab.show()
@@ -181,7 +108,7 @@ main_layout=QtWidgets.QVBoxLayout()
 main_layout.setContentsMargins(0, 0, 0, 0) # 去掉外邊框
 #main_layout.addLayout(bar_layout)
 main_layout.addWidget(bar)
-main_layout.addWidget(close_bar_rbutton)
+main_layout.addWidget(bar.close_bar_rbutton)
 new_tab_button=QtWidgets.QPushButton("new tab")
 def add_new_tab():
     b=QtWebEngineWidgets.QWebEngineView()
